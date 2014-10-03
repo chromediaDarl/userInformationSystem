fd88169e929b6404ebca01ef817597e3d2e3d916
chromediaDarl <kimberlydarl.barbadillo@chromedia.com>
2014-10-03 17:42:57 +0800

    Added Update Profile page and functionality

---
+++ src/UserManagement/UserMgtBundle/Form/CurrentUserType.php
@@ -0,0 +1,22 @@
+<?php
+// src/UserManagement/UserMgtBundle/Form/CurrentUserType.php
+
+namespace UserManagement\UserMgtBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class CurrentUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('fname', 'text', array('label' => false , 'attr' => array('placeholder' => 'First Name', 'class' => 'form-control')));
+        $builder->add('lname', 'text', array('label' => false , 'attr' => array('placeholder' => 'Last Name', 'class' => 'form-control')));
+        $builder->add('email', 'email', array('label' => false , 'read_only' => true , 'attr' => array('placeholder' => 'Email Address', 'class' => 'form-control')));
+    }
+
+    public function getName()
+    {
+        return 'currentuser';
+    }
+}
