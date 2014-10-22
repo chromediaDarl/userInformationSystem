92c520b382a75d2f35910c635aba44edb0081ee7
chromediaDarl <kimberlydarl.barbadillo@chromedia.com>
2014-10-22 11:15:05 +0800

    minor changes

--- src/UserManagement/UserMgtBundle/Form/UserType.php
+++ src/UserManagement/UserMgtBundle/Form/UserType.php
@@ -13,8 +13,8 @@ class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('fname', 'text', array('constraints' => array( new NotBlank(array('message' => 'First Name is requuired'))),'label' => false , 'attr' => array('placeholder' => 'First Name', 'class' => 'form-control')));
-        $builder->add('lname', 'text', array('constraints' => array( new NotBlank(array('message' => 'Last Name is requuired'))),'label' => false , 'attr' => array('placeholder' => 'Last Name', 'class' => 'form-control')));
+        $builder->add('fname', 'text', array('constraints' => array( new NotBlank(array('message' => 'First Name is required'))),'label' => false , 'attr' => array('placeholder' => 'First Name', 'class' => 'form-control')));
+        $builder->add('lname', 'text', array('constraints' => array( new NotBlank(array('message' => 'Last Name is required'))),'label' => false , 'attr' => array('placeholder' => 'Last Name', 'class' => 'form-control')));
         $builder->add('email', 'email', array('constraints' => array( new Email(array('message' => 'Invalid email'))),'label' => false , 'attr' => array('placeholder' => 'Email Address', 'class' => 'form-control')));
         $builder->add('password', 'password', array(
             'constraints' => array( new Length(array('min'=> 6, 'minMessage' => 'New Password must be at least {{ limit }} characters long'))),
